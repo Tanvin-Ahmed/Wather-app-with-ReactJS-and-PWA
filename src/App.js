333beddedb9792e126b1e189b4d12729c1e7e475
Tanvin Ahmed <tanvinahmedtouhid91@gmail.com>
2022-04-03 16:46:08 +0600

    add install button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import { fetchWeather } from "./apis/fetchWeather";
 import "./App.css";
 
 function App() {
 	const [query, setQuery] = useState("");
 	const [weather, setWeather] = useState({});
+	const deferredPrompt = useRef(null);
 
 	const search = async e => {
 		if (e.key === "Enter") {
@@ -18,35 +19,60 @@ function App() {
 		}
 	};
 
+	useEffect(() => {
+		const ins = e => {
+			deferredPrompt.current = e;
+		};
+		window.addEventListener("beforeinstallprompt", ins);
+		return () => window.addEventListener("beforeinstallprompt", ins);
+	}, []);
+
+	const handleInstall = async () => {
+		if (deferredPrompt.current !== null) {
+			deferredPrompt.current.prompt();
+			const { outcome } = await deferredPrompt.userChoice;
+			if (outcome === "accepted") {
+				deferredPrompt.current = null;
+			}
+		}
+	};
+
 	return (
-		<div className="main-container">
-			<input
-				type="text"
-				className="search"
-				placeholder="Search..."
-				value={query}
-				onChange={e => setQuery(e.target.value)}
-				onKeyUp={search}
-			/>
-			{weather.main && (
-				<div className="city">
-					<h2 className="city-name">
-						<span>{weather.name}</span>
-						<sup>{weather.sys.country}</sup>
-					</h2>
-					<div className="city-temp">
-						{weather.main.temp} <sup>℃</sup>
+		<div className="main">
+			<div style={{ textAlign: "right" }}>
+				<button type="button" className="install" onClick={handleInstall}>
+					Install
+				</button>
+			</div>
+			<div className="main-container">
+				<input
+					type="text"
+					className="search"
+					placeholder="Search..."
+					value={query}
+					onChange={e => setQuery(e.target.value)}
+					onKeyUp={search}
+				/>
+				{weather.main && (
+					<div className="city">
+						<h2 className="city-name">
+							<span>{weather.name}</span>
+							<sup>{weather.sys.country}</sup>
+						</h2>
+						<div className="city-temp">
+							{weather.main.temp} <sup>℃</sup>
+						</div>
+						<div className="info">
+							<img
+								src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+								alt={weather.weather[0].description}
+								className="city-icon"
+							/>
+							<p>{weather.weather[0].description}</p>
+						</div>
 					</div>
-					<div className="info">
-						<img
-							src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
-							alt={weather.weather[0].description}
-							className="city-icon"
-						/>
-						<p>{weather.weather[0].description}</p>
-					</div>
-				</div>
-			)}
+				)}
+			</div>
 		</div>
 	);
 }
